2343dd50a7a754da6c4cf2f8583a0e1947d89395
Ben <ben@unknown5c969d7163e7.gateway.2wire.net>
2013-10-02 20:14:02 -0700

    first commit

---
+++ ASD_TEMP/android/platforms/android/src/com/fullsail/testApp/tickName.java
@@ -0,0 +1,19 @@
+
+package com.fullsail.testApp;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class tickName extends DroidGap
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        // Set by <content src="index.html" /> in config.xml
+        //super.loadUrl(Config.getStartUrl());
+     
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
+
