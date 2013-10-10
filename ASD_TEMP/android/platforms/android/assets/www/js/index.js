fbfbd548f6ef0de549e4a97dfd2422210988d08a
Ben <ben@unknown5c969d7163e7.gateway.2wire.net>
2013-10-09 21:18:31 -0700

    2 week commit

--- ASD_TEMP/android/platforms/android/assets/www/js/index.js
+++ ASD_TEMP/android/platforms/android/assets/www/js/index.js
@@ -2,7 +2,8 @@
 Banchop Ben Kangdouangnhot
 Term 1310
 Class AVF
- */
+*/
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -32,5 +33,68 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+        
+       // $.getJSON(url, function(data){...});
+       
     }
+        
+    
 };
+
+/*  $(".instagram").instagram({
+        userId: 'USER-ID',
+        accessToken: 'ACCESS-TOKEN'
+    });
+ */   
+
+  
+$(function() {
+	var tag = "WRX";
+	var url = "https://api.instagram.com/v1/tags/" + tag + 
+		"/media/recent?callback=?&amp;client_id=3e73c63dfc2940588f99fbf372e319a8;min_id=10";
+	$.getJSON(url, pics);
+});
+
+
+var pics = function(info){
+
+	alert("Pictures loading");
+	console.log(info);
+
+	$("#msg").html("<h2>Instagram findings:</h2>");
+
+	$.each(info.data, function(index, photo){
+		var pic = "<li><img src='" + photo.images.standard_resolution.url + "' alt='" + photo.user.id
+		+ "' /><h4>" + photo.user.full_name + ", <em>(" + photo.user.username +")</em></h4></li>";
+		$("#data-output").append(pic);
+	});
+	
+};
+
+
+
+/*
+$(function() {
+
+    $.ajax({
+        type: "GET",
+        dataType: "jsonp",
+        cache: false,
+        url: "https://api.instagram.com/v1/users/[userid]/media/recent/?access_token=[access_token]",
+        success: function(data) {
+
+            for (var i = 0; i < 6; i++) {
+        $(".instagram").append("<div class='instagram-placeholder'>
+        <a target='_blank' href='" + data.data[i].link +"'><img class='instagram-image' src='" + data.data[i].images.thumbnail.url +"' /></a></div>");   
+                }     
+                            
+        }
+    });
+});
+*/
+
+
+
+
+
+
